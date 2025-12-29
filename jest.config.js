0f85503acbf723cf5997d779b3ec676ad9afa341
Jamie Hurst <jamie@jamiehurst.co.uk>
2025-12-29 21:26:46 +0000

    breaking: updated svelte to latest version (3.x to 5.x) - although this should have no breaking changes, this is just in case

--- jest.config.js
+++ jest.config.js
@@ -4,17 +4,28 @@ module.exports = {
   verbose: true,
   preset: 'ts-jest',
   transform: {
-    '^.+\\.ts$': ['ts-jest', {
-      tsconfig: {
-        module: 'CommonJS',
-        verbatimModuleSyntax: false,
-      }
-    }],
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          module: 'CommonJS',
+          verbatimModuleSyntax: false,
+        },
+      },
+    ],
+    '^.+\\.m?js$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          module: 'CommonJS',
+          verbatimModuleSyntax: false,
+          allowJs: true,
+        },
+      },
+    ],
   },
-  moduleFileExtensions: ['js', 'd.ts', 'ts'],
+  transformIgnorePatterns: ['node_modules/(?!(svelte|esm-env)/)'],
+  moduleFileExtensions: ['js', 'd.ts', 'ts', 'mjs'],
   coverageReporters: ['html', 'text', 'cobertura'],
-  collectCoverageFrom: [
-    'src/**/*.{js,jsx,ts,tsx}',
-    '!<rootDir>/node_modules/',
-  ],
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!<rootDir>/node_modules/'],
 };
